fd168f53f22c9c065c3201176b52c64052260b87
Ileana-B4 <ileana.tapia@uicui.edu.mx>
2022-07-22 20:39:39 -0500

    actualización del Carousel

--- src/components/Carousel/CarouselContainer.jsx
+++ src/components/Carousel/CarouselContainer.jsx
@@ -1,69 +1,55 @@
 import React from 'react'; 
 import Carousel from 'react-bootstrap/Carousel';
+import "./CarouselContainer.css"
+import image1 from "../../media/image-1.jpg"
+import image2 from "../../media/image-2.jpg"
+import image3 from "../../media/image-3.jpg"
+import image4 from "../../media/image-4.jpg"
+import image5 from "../../media/image-5.png"
 
 const CarouselContainer = () => {
     return(
-        <Carousel fade>
+      <div className="carousel-container">
+      <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://i.ibb.co/c3sYPHz/image-2.jpg"
-          alt="CCH"
+          className="image"
+          src={image1}
+          alt="First slide"
         />
         <Carousel.Caption>
-          <h3>XXXIV Concurso Intra-CCH de Matemáticas</h3>
-          <p>CCH-Naucalpan</p>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          className="image"
+          src={image2}
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Primer Lugar</h3>
-          <p>XXXIV Concurso Intra-CCH de Matemáticas</p>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          className="image"
+          src={image3}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Olimpiada Estatal de Biologia</h3>
+          <h3>Third slide label</h3>
           <p>
-            UAMEX estado de México
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Tercer Lugar OEB</h3>
-          <p>Olimpiada Estatal de Biologia</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Prototipo de Robot Minisumo</h3>
-          <p>Con ayuda del ingeniero Hugo Angeles</p>
-        </Carousel.Caption>
-      </Carousel.Item>     
     </Carousel>
+    </div>
     )
 
 }
--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -12,4 +12,4 @@ const Cover = () => {
     );
 };
  
-export default Cover;
+export default Cover
